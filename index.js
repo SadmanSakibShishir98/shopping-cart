6f9d44494485c94f8bca11feba8c760259bd5161
SadmanSakibShishir98 <sadmansakibshishir98@gmail.com>
2021-07-23 21:35:33 +0600

    2nd edit

--- index.js
+++ index.js
@@ -1,73 +1,43 @@
+document.getElementById('case-increase').addEventListener('click', function(){
+    let formType='case-count';
+    let totalType='case-total';
+  handleProductChange(true,formType,totalType);
 
-var subTotal=parseFloat(document.getElementById('iphonePrice').innerText)+parseFloat(document.getElementById('casePrice').innerText);
-const btn1Minus= document.getElementById('btn1Minus');
-btn1Minus.addEventListener('click', function(){
-   
-    const totalPhones=parseFloat(document.getElementById('iphoneForm').value);
-    
-    
-    document.getElementById('subTotal').innerText=subTotal;
-    if(totalPhones>1 || totalPhones==1){
-        document.getElementById('iphoneForm').value=totalPhones;
-        var totalPhonePrice= parseFloat(document.getElementById('iphonePrice').innerText)/parseFloat(document.getElementById('iphoneForm').value);
-        document.getElementById('iphonePrice').innerText=totalPhonePrice;
-      
-        
-        if(totalPhones>=2) {
-        document.getElementById('iphoneForm').value=parseFloat(document.getElementById('iphoneForm').value)-1
-        }
-       else if(totalPhones<=1){
-        document.getElementById('iphoneForm').value=parseFloat(document.getElementById('iphoneForm').value)=1;
-       }
-   
-    
+});
 
+document.getElementById('case-decrease').addEventListener('click', function(){
+    let formType='case-count';
+    let totalType='case-total';
+    handleProductChange(false,formType,totalType);
+    
+})
+
+document.getElementById('phone-increase').addEventListener('click', function(){
+    let formType='phone-count';
+    let totalType='phone-total';
+  handleProductChange(true,formType,totalType);
+
+});
+
+document.getElementById('phone-decrease').addEventListener('click', function(){
+    let formType='phone-count';
+    let totalType='phone-total'
+   handleProductChange(false,formType,totalType);
+    
+});
+
+
+function handleProductChange(isIncrease,itemCount ,total){
+    const productInput= document.getElementById(itemCount);
+    const productCount =parseInt(productInput.value);
+    let productNewCount=productCount
+    if(isIncrease==true){
+        productNewCount=productCount+1;
+    }
+    if(isIncrease==false && productCount>0){
+        productNewCount= productCount-1;
+    }
+    productInput.value=productNewCount;
+    const productTotal=productNewCount*59;
+    document.getElementById(total).innerText=productTotal;
 }
-})
-
-const btn1Plus= document.getElementById('btn1Plus');
-btn1Plus.addEventListener('click', function(){
-   
-    
-    document.getElementById('subTotal').innerText=subTotal;
-    const totalPhones=parseFloat(document.getElementById('iphoneForm').value)+1;
-   
-        document.getElementById('iphoneForm').value=totalPhones;
-        var totalPhonePrice= parseFloat(document.getElementById('iphonePrice').innerText)*totalPhones;
-        document.getElementById('iphonePrice').innerText=totalPhonePrice;
- 
-    
-})
-
-
-const btn2Minus= document.getElementById('btn2Minus');
-btn2Minus.addEventListener('click', function(){
-const totalCase=parseFloat(document.getElementById('caseForm').value);
-if(totalCase>=1){
-    document.getElementById('caseForm').value=totalCase;
-    var totalCasePrice=parseFloat(document.getElementById('casePrice').innerText)/parseFloat(document.getElementById('caseForm').value);
-    document.getElementById('casePrice').innerText=totalCasePrice;
-    if(totalCase>=2) {
-        document.getElementById('caseForm').value=parseFloat(document.getElementById('caseForm').value)-1
-        }
-       else if(totalCase<=1){
-        document.getElementById('caseForm').value=parseFloat(document.getElementById('caseForm').value)=1;
-       }
-
-
-}
-})
-
-const btn2Plus= document.getElementById('btn2Plus');
-btn2Plus.addEventListener('click', function(){
-    const totalCase=parseFloat(document.getElementById('caseForm').value)+1;
-   
-        document.getElementById('caseForm').value=totalCase;
-        var totalCasePrice= parseFloat(document.getElementById('casePrice').innerText)*totalCase;
-        document.getElementById('casePrice').innerText=totalCasePrice;
- 
-    
-})
-
-
-
